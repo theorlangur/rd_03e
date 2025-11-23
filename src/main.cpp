71a49a0181bb423e8f62d2829081b2cb10c8c746
TheOrlangur <theorlangur@gmail.com>
2025-11-23 14:37:22 +0100

    cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -112,25 +112,6 @@ int main(void)
 		k_msleep(SLEEP_TIME_MS);
 	}
 
-	//std::unique_ptr<int> ptr;
-	//std::shared_ptr<char> ptr2;
-	//std::cout << "Hello, C++ world! " << CONFIG_BOARD << std::endl;
-	//return (int)ptr.get();
 	return 0;
 }
 
-
-namespace std
-{
-	inline namespace __1{
-		[[noreturn]] void __libcpp_verbose_abort(char const* format, ...) noexcept {
-			{
-				va_list list;
-				va_start(list, format);
-				vprintk(format, list);
-				va_end(list);
-			}
-			k_sleep(K_FOREVER);
-		}
-	}
-}
