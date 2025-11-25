e8daeaad693e3edc2c778faa8b1e4b43cdc20065
TheOrlangur <theorlangur@gmail.com>
2025-11-25 20:39:04 +0100

    bringing first zigbee boilerplate

--- src/main.cpp
+++ src/main.cpp
@@ -4,22 +4,79 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <optional>
 #define FORCE_FMT
 #define PRINTF_FUNC(...) printk(__VA_ARGS__)
-
-//#include <iostream>
-#include <memory>
 
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/uart.h>
-#include "lib/lib_uart.h"
+#include <zephyr/settings/settings.h>
 #include "lib/lib_dfr_c4001.h"
 
-/* 1000 msec = 1 sec */
-#define SLEEP_TIME_MS   5000
+/**********************************************************************/
+/* Zigbee                                                             */
+/**********************************************************************/
+#include <nrfzbcpp/zb_main.hpp>
+#include <nrfzbcpp/zb_std_cluster_desc.hpp>
+#include <nrfzbcpp/zb_status_cluster_desc.hpp>
 
+/**********************************************************************/
+/* Zigbee Declarations and Definitions                                */
+/**********************************************************************/
+static bool g_ZigbeeReady = false;
+
+/* Manufacturer name (32 bytes). */
+#define INIT_BASIC_MANUF_NAME      "SFINAE"
+
+/* Model number assigned by manufacturer (32-bytes long string). */
+#define INIT_BASIC_MODEL_ID        "C4001-NG"
+
+
+/* Button used to enter the Bulb into the Identify mode. */
+#define IDENTIFY_MODE_BUTTON            DK_BTN1_MSK
+
+/* Button to start Factory Reset */
+#define FACTORY_RESET_BUTTON IDENTIFY_MODE_BUTTON
+
+/* Device endpoint, used to receive light controlling commands. */
+constexpr uint8_t kMMW_EP = 1;
+constexpr uint16_t kDEV_ID = 0xBAAD;
+
+struct device_ctx_t{
+    zb::zb_zcl_basic_names_t basic_attr;
+    zb::zb_zcl_status_t status_attr;
+};
+
+//attribute shortcuts for template arguments
+constexpr auto kAttrStatus1 = &zb::zb_zcl_status_t::status1;
+constexpr auto kAttrStatus2 = &zb::zb_zcl_status_t::status2;
+constexpr auto kAttrStatus3 = &zb::zb_zcl_status_t::status3;
+
+/* Zigbee device application context storage. */
+static constinit device_ctx_t dev_ctx{
+    .basic_attr = {
+	{
+	    .zcl_version = ZB_ZCL_VERSION,
+	    .power_source = zb::zb_zcl_basic_min_t::PowerSource::DC
+	},
+	/*.manufacturer =*/ INIT_BASIC_MANUF_NAME,
+	/*.model =*/ INIT_BASIC_MODEL_ID,
+    },
+};
+
+constinit static auto zb_ctx = zb::make_device(
+	zb::make_ep_args<{.ep=kMMW_EP, .dev_id=kDEV_ID, .dev_ver=1}>(
+	    dev_ctx.basic_attr
+	    , dev_ctx.status_attr
+	    )
+	);
+
+//a shortcut for a convenient access
+constinit static auto &zb_ep = zb_ctx.ep<kMMW_EP>();
+
+/**********************************************************************/
+/* Device defines                                                     */
+/**********************************************************************/
 /* The devicetree node identifier for the "led0" alias. */
 #define LED0_NODE DT_ALIAS(led0)
 
@@ -47,7 +104,39 @@ void presence_triggered(const struct device *port,
 	gpio_pin_set_dt(&led, val);
 }
 
+void on_dev_cb_error(int err)
+{
+    printk("on_dev_cb_error: %d\r\n", err);
+}
+
+void test_func();
+
 int main(void)
+{
+    int err = settings_subsys_init();
+    err = settings_load();
+
+    /* Register callback for handling ZCL commands. */
+    auto dev_cb = zb::tpl_device_cb<
+		zb::dev_cb_handlers_desc{ .error_handler = on_dev_cb_error }
+    >;
+
+    ZB_ZCL_REGISTER_DEVICE_CB(dev_cb);
+
+    /* Register dimmer switch device context (endpoints). */
+    ZB_AF_REGISTER_DEVICE_CTX(zb_ctx);
+
+    zigbee_enable();
+    printk("Main: sleep forever\r\n");
+	test_func();
+    while (1) {
+		k_sleep(K_FOREVER);
+    }
+
+	return 0;
+}
+
+void test_func()
 {
 	FMT_PRINTLN("main. start");
 	k_msleep(1000);
@@ -96,9 +185,7 @@ int main(void)
 	gpio_init_callback(&g_cb, presence_triggered, BIT(presence.pin));
 	gpio_add_callback_dt(&presence, &g_cb);
 
-	printk("sleeping...\r\n");
-	while(true)
-		k_msleep(100000);
-	return 0;
+	//printk("sleeping...\r\n");
+	//while(true)
+	//	k_msleep(100000);
 }
-
