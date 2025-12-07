44661e8b44ddf16f36865f7815c21857d6758e3b
TheOrlangur <theorlangur@gmail.com>
2025-12-07 11:47:50 +0100

    using legacy zigbee for nrf52840
    still bricked one...

--- build-52840-llvm-lcxx.sh
+++ build-52840-llvm-lcxx.sh
@@ -2,7 +2,7 @@
 west build --build-dir build52 . --pristine \
     --board promicro_nrf52840/nrf52840/uf2 -- \
     -DBOARD_ROOT=~/myapps/cpp/nrf \
-    -DCONF_FILE="prj.conf config/cpp_lcxx.conf config/nrf52.conf config/zb.conf config/no_log.conf" \
+    -DCONF_FILE="prj.conf config/cpp_lcxx.conf config/nrf52.conf config/zb_legacy.conf config/no_log.conf" \
     -DZEPHYR_TOOLCHAIN_VARIANT=llvm \
     -DCONFIG_LLVM_USE_LLD=y \
     -DCONFIG_COMPILER_RT_RTLIB=y \
